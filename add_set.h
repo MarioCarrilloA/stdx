5bd074e2b4dfe5b59dde49f165f2cf9919bd9fe5
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2020-01-03 21:20:10 -0700

    Add header to process data sets
    This header includes code to parse and process the data sets.
    
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

---
+++ add_set.h
@@ -0,0 +1,15 @@
+#ifndef __ADD_SET_H__
+#define __ADD_SET_H__
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void add_set();
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* __ADD_SET_H__ */
